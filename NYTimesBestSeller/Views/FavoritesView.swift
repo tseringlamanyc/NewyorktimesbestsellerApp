0423e81c76ae523e61f0f7473af554c166867e3a
Kelby Mittan <kelbymittan@pursuit.org>
2020-02-05 16:50:33 -0500

    first commit

--- NYTimesBestSeller/Views/FavoritesView.swift
+++ NYTimesBestSeller/Views/FavoritesView.swift
@@ -10,12 +10,6 @@ import UIKit
 
 class FavoritesView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    // TODO: Kelby is going to work on Favorites View
 
 }
