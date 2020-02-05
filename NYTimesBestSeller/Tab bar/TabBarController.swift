c72c8766adc7eb72d731875c8e8c3e5c077de60e
Tsering Lama <tseringlama@pursuit.org>
2020-02-05 16:19:45 -0500

    added tab bar

--- NYTimesBestSeller/Tab bar/TabBarController.swift
+++ NYTimesBestSeller/Tab bar/TabBarController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class TabBarController: UITabBarController {
+    
+    private var bestSellerVC: BestSellerViewController = {
+       let vc = BestSellerViewController()
+        vc.tabBarItem = UITabBarItem(title: "1", image: UIImage(systemName: "magnifyingglass"), tag: 0)
+        return vc
+    }()
+    
+    private var favoritesVC: FavoritesViewController = {
+       let vc = FavoritesViewController()
+        vc.tabBarItem = UITabBarItem(title: "2", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        return vc
+    }()
 
+    private var settingVC: SettingsViewController = {
+        let vc = SettingsViewController()
+        vc.tabBarItem = UITabBarItem(title: "3", image: UIImage(systemName: "magnifyingglass"), tag: 2)
+        return vc
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        viewControllers = [bestSellerVC, favoritesVC, settingVC]
     }
 }
