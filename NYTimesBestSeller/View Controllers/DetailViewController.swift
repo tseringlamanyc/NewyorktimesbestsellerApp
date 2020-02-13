ee5c890adcd4390c4e5329c16e32f00a10e021b4
Margiett Gil <mxg627@icloud.com>
2020-02-13 17:08:28 -0500

    did the settings and detail controller

--- NYTimesBestSeller/View Controllers/DetailViewController.swift
+++ NYTimesBestSeller/View Controllers/DetailViewController.swift
@@ -7,17 +7,112 @@
 //
 
 import UIKit
+import DataPersistence
 
 class DetailViewController: UIViewController {
-//TODO: Margiett will be working on this view controller
     
-    // testing 
+    private var book: Book
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    private let detailView = DetailView()
+    
+    private var dataPersistence: DataPersistence<Book>
+    
+    
+    private var bookmarkBarButton: UIBarButtonItem!
+    
+    init(_ dataPersistence: DataPersistence<Book>, books: Book) {
+        self.dataPersistence = dataPersistence
+        self.book = books
+        super.init(nibName: nil, bundle: nil)
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func loadView() {
+      view = detailView
+    }
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      view.backgroundColor = .systemBackground
+      
+      // adding a UIBarButtonItem to the right side to the navigation bar's title
+      bookmarkBarButton = UIBarButtonItem(image: UIImage(systemName: "bookmark"), style: .plain, target: self, action: #selector(saveArticleButtonPressed(_:)))
+      navigationItem.rightBarButtonItem = bookmarkBarButton
+      
+      updateUI()
+    }
+    
+    private func updateUI() {
+     
+//      guard let updateBook = book else {
+//        fatalError("did not load an article")
+//      }
+      updateBookmarkState(book)
+        navigationItem.title = book.title
+     //detailView.abstractHeadline.text = book.abstract
+      
+        detailView.newsImageView.getImage(with: book.bookImage) { [weak self] (result) in
+        switch result {
+        case .failure:
+          DispatchQueue.main.async {
+            self?.detailView.newsImageView.image = UIImage(systemName: "exclamationmark-octogon")
+          }
+        case .success(let image):
+          DispatchQueue.main.async {
+            self?.detailView.newsImageView.image = image
+          }
+        }
+      }
+        
+        GoogleAPIClient.getGoogleBooks(for: book.primaryIsbn10) { (result) in
+            switch result {
+            case .failure(let appError):
+                print("error \(appError)")
+            case .success(let book):
+                DispatchQueue.main.async {
+                    self.detailView.summary.text = book.first?.volumeInfo.description ?? "N/A"
+                    self.detailView.abstractHeadline.text = book.first?.volumeInfo.title ?? "N/A"
+                }
+            }
+        }
+        
+        
+    }
+    
+    @objc func saveArticleButtonPressed(_ sender: UIBarButtonItem) {
+////      guard let article = book else { return }
+//      // ADDITION: check for duplicates
+//      if dataPersistence.hasItemBeenSaved(book) {
+//        if let index = try? dataPersistence.loadItems().firstIndex(of: book) {
+//          do {
+//            try dataPersistence.deleteItem(at: index)
+//          } catch {
+//            print("error deleting article: \(error)")
+//          }
+//        }
+//      } else {
+        do {
+          // save to documents directory
+          try dataPersistence.createItem(book)
+        } catch {
+          print("error saving article: \(error)")
+        }
 
+      // ADDITION:
+      // update bookmark state
+      updateBookmarkState(book)
+    }
+
+    // ADDITION
+    private func updateBookmarkState(_ article: Book) {
+      if dataPersistence.hasItemBeenSaved(article) {
+        bookmarkBarButton.image = UIImage(systemName: "bookmark.fill")
+      } else {
+        bookmarkBarButton.image = UIImage(systemName: "bookmark")
+      }
+    }
 }
