683df78ddb2168d6106a41ad3201230df4992a19
Kelby Mittan <kelbymittan@pursuit.org>
2020-02-13 12:12:35 -0500

    made new files and updated favorites

--- NYTimesBestSeller/Cells/FavoritesCell.swift
+++ NYTimesBestSeller/Cells/FavoritesCell.swift
@@ -8,7 +8,116 @@
 
 import UIKit
 import Gemini
+import ImageKit
+
+protocol GeminiCellDelegate: AnyObject {
+    func didLongPress(_ imageCell: FavoritesCell)
+}
 
 class FavoritesCell: GeminiCell {
-    
-}
+    public lazy var bookImageView: UIImageView = {
+            let iv = UIImageView()
+            iv.image = UIImage(systemName: "photo")
+            iv.contentMode = .scaleAspectFill
+            iv.backgroundColor = .systemTeal
+            return iv
+        }()
+        
+        public lazy var bookTitle: UILabel = {
+            let label = UILabel()
+            label.numberOfLines = 2
+            label.font = UIFont.preferredFont(forTextStyle: .headline)
+            label.text = "Book Title"
+            return label
+        }()
+        
+        public lazy var bookDescription: UILabel = {
+            let label = UILabel()
+            label.numberOfLines = 4
+            label.font = UIFont.preferredFont(forTextStyle: .subheadline)
+            label.text = "Book Description"
+            return label
+        }()
+        
+        public lazy var selectedView: UIView = {
+            let view = UIView()
+            view.backgroundColor = .lightGray
+            view.isHidden = true
+            return view
+        }()
+        
+        weak var geminiDelegate: GeminiCellDelegate?
+        
+        override init(frame: CGRect) {
+            super.init(frame: UIScreen.main.bounds)
+            commonInit()
+        }
+        
+        required init?(coder: NSCoder) {
+            super.init(coder: coder)
+            commonInit()
+        }
+        
+        private func commonInit() {
+            setupImageConstraints()
+            setupSelectedViewConstraints()
+        }
+        
+        private lazy var longPressGesture: UILongPressGestureRecognizer = {
+            let gesture = UILongPressGestureRecognizer()
+            gesture.addTarget(self, action: #selector(longPressAction(gesture:)))
+            return gesture
+        }()
+        
+        override func layoutSubviews() {
+            super.layoutSubviews()
+            
+            addGestureRecognizer(longPressGesture)
+        }
+        
+        private func setupImageConstraints() {
+            addSubview(bookImageView)
+            bookImageView.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                bookImageView.topAnchor.constraint(equalTo: topAnchor),
+                bookImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+                bookImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+                bookImageView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            ])
+        }
+        
+        private func setupSelectedViewConstraints() {
+            addSubview(selectedView)
+            selectedView.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                selectedView.centerXAnchor.constraint(equalTo: bookImageView.centerXAnchor),
+                selectedView.centerYAnchor.constraint(equalTo: bookImageView.centerYAnchor),
+                selectedView.widthAnchor.constraint(equalTo: bookImageView.widthAnchor, multiplier: 0.75),
+                selectedView.heightAnchor.constraint(equalTo: bookImageView.heightAnchor, multiplier: 0.75)
+            ])
+        }
+        
+        public func configureCell(for book: Book) {
+            bookImageView.getImage(with: book.bookImage) {[weak self] (result) in
+                switch result {
+                case .failure:
+                    self?.bookImageView.image = UIImage(systemName: "photo")
+                case .success(let image):
+                    DispatchQueue.main.async {
+                        self?.bookImageView.image = image
+                    }
+                }
+            }
+        }
+        
+        @objc private func longPressAction(gesture: UILongPressGestureRecognizer) {
+            if gesture.state == .began {
+                gesture.state = .cancelled
+                return
+            }
+            geminiDelegate?.didLongPress(self)
+        }
+
+    }
--- NYTimesBestSeller/Views/FavoritesView.swift
+++ NYTimesBestSeller/Views/FavoritesView.swift
@@ -7,9 +7,54 @@
 //
 
 import UIKit
+import Gemini
 
 class FavoritesView: UIView {
-
-    // TODO: Kelby is going to work on Favorites View
-
+    
+    public lazy var geminiCollectionView: GeminiCollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        
+        layout.scrollDirection = .vertical
+        
+        layout.itemSize = CGSize(width: 120, height: 200)
+        let cv = GeminiCollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        //        cv.backgroundColor = .darkGray
+        //        cv.isHidden = true
+        return cv
+    }()
+    
+    public lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        return cv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setupGeminiCollectionViewConstraints()
+    }
+    
+    private func setupGeminiCollectionViewConstraints() {
+        addSubview(geminiCollectionView)
+        geminiCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            geminiCollectionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            geminiCollectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            geminiCollectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            geminiCollectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
 }
+
