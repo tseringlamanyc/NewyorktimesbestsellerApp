283a7a1eb6c1de36a221f51ab67f3d3c9cc8c043
Tsering Lama <tseringlama@pursuit.org>
2020-02-14 15:56:22 -0500

    user defaults fully functions

--- NYTimesBestSeller/View Controllers/SettingsViewController.swift
+++ NYTimesBestSeller/View Controllers/SettingsViewController.swift
@@ -10,78 +10,82 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
-private let settingsView = SettingsView()
-  
-  public var userPreference: UserPreference!
+    private let settingsView = SettingsView()
     
-  override func loadView() {
-    view = settingsView
-  }
+    public var userPreference: UserPreference!
+    
+    override func loadView() {
+        view = settingsView
+    }
     
     var sections = [String]() {
-          didSet {
-              settingsView.pickerViewSelected.reloadAllComponents()
-          }
-      }
-      
-      private var allCategories = [Categories]() {
-          didSet {
+        didSet {
+            settingsView.pickerViewSelected.reloadAllComponents()
+            getIndex()
+        }
+    }
+    
+    private var allCategories = [Categories]() {
+        didSet {
             sections = allCategories.map {$0.listName}.sorted()
-          }
-      }
+        }
+    }
+    
+    private func getIndex() {
+        if let sectionName = userPreference.getSectionName() {
+            if let index = sections.firstIndex(of: sectionName) {
+                settingsView.pickerViewSelected.selectRow(index, inComponent: 0, animated: true)
+            }
+        }
+    }
     
     private func getCategories() {
-         CategoryAPI.getCategory { [weak self] (result) in
-             switch result {
-             case .failure(_):
-                 print("Couldnt get categories")
-             case .success(let data):
-                 DispatchQueue.main.async {
-                     self?.allCategories = data
-                 }
-             }
-         }
-     }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    view.backgroundColor = .systemGroupedBackground
+        CategoryAPI.getCategory { [weak self] (result) in
+            switch result {
+            case .failure(_):
+                print("Couldnt get categories")
+            case .success(let data):
+                DispatchQueue.main.async {
+                    self?.allCategories = data
+                }
+            }
+        }
+    }
     
-    // setup picker view
-    settingsView.pickerViewSelected.dataSource = self
-    settingsView.pickerViewSelected.delegate = self
-    getCategories()
-    
-    // ADDITION: scroll to picker view's index if there is a section saved in UserDefaults
-//    if let sectionName = userPreference.getSectionName() {
-//      if let index = sections.firstIndex(of: sectionName) {
-//        settingsView.pickerViewSelected.selectRow(index, inComponent: 0, animated: true)
-//      }
-//    }
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGroupedBackground
+        
+        // setup picker view
+        settingsView.pickerViewSelected.dataSource = self
+        settingsView.pickerViewSelected.delegate = self
+        getCategories()
+        
+        // ADDITION: scroll to picker view's index if there is a section saved in UserDefaults
+    }
 }
 
 extension SettingsViewController: UIPickerViewDataSource {
-  func numberOfComponents(in pickerView: UIPickerView) -> Int {
-    return 1
-  }
-  
-  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-    return sections.count
-  }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return sections.count
+    }
 }
 
 extension SettingsViewController: UIPickerViewDelegate {
-  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-    return sections[row] // accessing each individual string in the sections array
-  }
-  
-  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-    // store the current selected news section in user defaults
-    let sectionName = sections[row]
-    userPreference.setSectionName(sectionName)
-    userPreference.setIndex(row)
-  }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return sections[row] // accessing each individual string in the sections array
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // store the current selected news section in user defaults
+        let sectionName = sections[row]
+        userPreference.setSectionName(sectionName)
+        userPreference.setIndex(row)
+    }
 }
 
 
